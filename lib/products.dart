602f2cbab45c511e96a3f1c63399c241e8cf94a1
mnowak <mateusz.nowak@intenso.pl>
2018-12-30 23:01:01 +0100

    Section 8: Lecture 105. Setting Borders

--- lib/products.dart
+++ lib/products.dart
@@ -18,7 +18,33 @@ class Products extends StatelessWidget {
         child: Column(
           children: <Widget>[
             Image.asset(products[index].image),
-            Text(products[index].title),
+            Padding(
+                padding: EdgeInsets.only(top: 10.0),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text(products[index].title,
+                      style: TextStyle(
+                          fontSize: 26.0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Oswald')),
+                  SizedBox(width: 8.0),
+                  Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 2.5, horizontal: 6.0),
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).accentColor,
+                          borderRadius: BorderRadius.circular(5.0)),
+                      child: Text(
+                        '\$' + products[index].price.toString(),
+                        style: TextStyle(color: Colors.white),
+                      ))
+                ])),
+            Container(
+                padding: EdgeInsets.symmetric(vertical: 2.5, horizontal: 6.0),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 1.0),
+                    borderRadius: BorderRadius.circular(4.0)),
+                child: Text('Union Square, San Francisco')),
             ButtonBar(alignment: MainAxisAlignment.center, children: [
               FlatButton(
                   child: Text('Details'),
@@ -26,7 +52,7 @@ class Products extends StatelessWidget {
                       Navigator.pushNamed<bool>(context, Routes.PRODUCT(index))
                           .then((bool value) {
                         if (value) {
-                         // deleteProduct(index);
+                          // deleteProduct(index);
                         }
                       }))
             ])
